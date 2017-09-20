ff0b3fe3866c34f0cc360e54881126eb9aab7ee0
BuildTools <mods.itsmeow@gmail.com>
2017-09-20 13:10:47 -0400

    Update to 1.12.2, very few changes lol

--- java/its_meow/cloneland/Ref.java
+++ java/its_meow/cloneland/Ref.java
@@ -3,7 +3,7 @@ package its_meow.cloneland;
 public class Ref { //Class used to easily reference variables that show up often. Pretty understandable for a begginer in programming, so I won't explain. It's not worth the time if you don't understand.
 	public static final String MOD_ID = "cloneland";
 	public static final String NAME = "Clone Land";
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.6";
 	public static final String CLIENT_PROXY_C = "its_meow.cloneland.proxy.ClientProxy";
 	public static final String SERVER_PROXY_C = "its_meow.cloneland.proxy.ServerProxy";
 	public static final String acceptedMCV = "[1.12,1.13)";
